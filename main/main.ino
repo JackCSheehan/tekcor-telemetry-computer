1eb9f2b5cff683b9eed9479cb9016d6d4f58c87e
Jack Sheehan <jackcsheehan@users.noreply.github.com>
2022-04-02 18:02:52 -0500

    Removed GPS code and finalized CSV writing

--- main/main.ino
+++ main/main.ino
@@ -1,118 +1,46 @@
 #include <Adafruit_BMP085.h>
 #include <SD.h>
-#include <TinyGPS++.h>
-#include <SoftwareSerial.h>
 
-static const int BAUD = 9600;
-static const char SD_PIN = 12;
+static const float SEA_LEVEL_PRES = 101625.52;
+static const char SD_PIN = 10;
 static const char DELIM = ',';
-
-// Serial pins
-static const char TXD_PIN = 6;
-static const char RXD_PIN = 5;
-
-// Values indicating errors in GPS reading
-static const int POS_ERR = 361;
-static const char TIME_ERR = -1;
 
 // Hardware library objects
 Adafruit_BMP085 bmp;
-TinyGPSPlus gps;
-SoftwareSerial ss(RXD_PIN, TXD_PIN);
 
 // Telemtry output file
 File telem;
 
-// Struct to hold pressure and temp data
+// Struct to hold pressure, temp, and altitude data
 struct BMPData {
     float pres;
     float temp;
-};
-
-// Struct to contain position data received from GPS at a particular time
-struct GPSData {
-    // Position
-    double lat;
-    double lon;
-    double alt;
-
-    // Time
-    char hour;
-    char min;
-    char sec;
-    char cent;
-
-    // Velocity
-    double vel;
+    float alt;
 };
 
 // Reads data from the BMP and returns it as a BMPData struct
 BMPData readBMPData() {
     BMPData presTemp = {
-        bmp.readPressure(),
-        bmp.readTemperature()
+       bmp.readPressure(),
+        bmp.readTemperature(),
+        bmp.readAltitude(SEA_LEVEL_PRES),
     };
     return presTemp;
 }
 
-// Writes data from GPSData and BMPData structs to the telemtry file
-void writeData(BMPData bmpData, GPSData gpsData) {
-    telem.print("0,");
-    telem.print(bmpData.temp); telem.print(DELIM);
-    telem.print(gpsData.alt); telem.print(DELIM);
-    telem.print(bmpData.pres); telem.print(DELIM);
-    telem.print(gpsData.vel); telem.print(DELIM);
-    telem.print(gpsData.lat); telem.print(DELIM);
-    telem.print(gpsData.lat); telem.print(DELIM);
-}
-
-// Reads data from GPS and returns as GPSData struct
-GPSData readGPSData() {
-    // Initialize to error values initially
-    GPSData pos = {POS_ERR, POS_ERR, TIME_ERR, TIME_ERR, TIME_ERR, TIME_ERR, TIME_ERR};
-
-    // Read if available
-    if (ss.available() > 0) {
-        if (gps.encode(ss.read())) {
-            // Read location
-            if (gps.location.isValid()) {
-                pos.lat = gps.location.lat();
-                pos.lon = gps.location.lng();
-            }
-
-            // Read altitude
-            if (gps.altitude.isValid()) {
-                pos.alt = gps.altitude.meters();
-            }
-
-            // Read velocity
-            if (gps.speed.isValid()) {
-                pos.vel = gps.speed.mps();
-            }
-
-            // Read time
-            if (gps.time.isValid()) {
-                pos.hour = gps.time.hour();
-                pos.min = gps.time.minute();
-                pos.sec = gps.time.second();
-                pos.cent = gps.time.centisecond();
-            }
-        }
-    }
-
-    return pos;
+// Writes data from BMPData struct to the telemtry file
+void writeData(BMPData bmpData) {
+    telem.print(millis() / 1000.0, 2); telem.print(DELIM);
+    telem.print(bmpData.temp, 2); telem.print(DELIM);
+    telem.print(bmpData.alt, 2); telem.print(DELIM);
+    telem.print(bmpData.pres, 2); telem.println(DELIM);
+    telem.flush();
 }
 
 // Prints values in given BMPData and GPSData structs
-void debug(GPSData gpsData, BMPData bmpData) {
-    Serial.print("LAT:");
-    Serial.println(gpsData.lat);
-
-    Serial.print("LON:");
-    Serial.println(gpsData.lon);
-
+void debug(BMPData& bmpData) {
     Serial.print("ALT:");
-    Serial.println(gpsData.alt);
+    Serial.println(bmpData.alt);
 
     Serial.print("PRES:");
     Serial.println(bmpData.pres);
@@ -123,43 +51,38 @@ void debug(GPSData gpsData, BMPData bmpData) {
 
 void setup() {
     Serial.begin(115200);
-    ss.begin(9600);
 
     // Initialize all external devices
     if (!SD.begin(SD_PIN)) {
-        Serial.println('S');
+        Serial.println("ERROR: Failed to initialize SD card");
         while(1);
     }
 
-    // Wait for GPS to initialize
-    delay(5000);
-
-    // If GPS hasn't processed enough data after 5 seconds, trigger an error
-    if (gps.charsProcessed() < 10) {
-        Serial.println('G');
-        while(1);
+    // If telemtry file already exists, computer was reset after flight. Enter wait loop
+    if (SD.exists("t.csv")) {
+      Serial.println("Entering reset hold");
+      while(1);
     }
 
     if (!bmp.begin()) {
-        Serial.println('B');
+        Serial.println("ERROR: Failed to initialize BMP");
         while(1);
     }
 
     // Create telemtry file and write CSV header
-    telem = SD.open("t.csv", FILE_WRITE);
+    telem = SD.open("t.csv", (O_WRITE | O_CREAT));
 
     if (telem) {
-        // Time, temperature, altitude, pressure, velocity, latitude, longitude
-        telem.println("t,tm,a,p,v,la,lo");
+        // Time, temperature, altitude, pressure
+        telem.println("t,c,a,p");
     } else {
-        Serial.println('F');
+        Serial.println("ERROR: Failed to open telemtry file");
         while(1);
     }
 }
 
 void loop() {
     BMPData bmpData = readBMPData();
-    GPSData gpsData = readGPSData();
-    debug(gpsData, bmpData);
-    writeData(bmpData, gpsData);
+    debug(bmpData);
+    writeData(bmpData);
 }
